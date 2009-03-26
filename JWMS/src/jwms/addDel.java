6b6dd84bd3d88d3efd5823564fb63da7ef660bba
lqik2004 <lqik2004@0a4000ee-19f5-11de-b69a-45dc1c5e4070>
2009-03-26 12:32:34 +0000

    jwms

---
+++ JWMS/src/jwms/addDel.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jwms;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrator
+ */
+public class addDel {
+
+    private String info;
+    private String color = "";
+    private String size = "";
+    private double inPrice = 0;
+    private double outPrice = 0;
+    private String store;
+    private int amount = 0;
+    //get some necessary information
+    public void setInfo(String text) {
+        info = text;
+    }
+
+    public void setColor(String text) {
+        color = text;
+    }
+
+    public void setSize(String text) {
+        size = text;
+    }
+
+    public void setAvgInPrice(double text) {
+        inPrice = text;
+    }
+
+    public void setAvgOutPrice(double text) {
+        outPrice = text;
+    }
+
+    public void setStore(String text) {
+        store = text;
+    }
+
+    public void setAmount(int text) {
+        amount = text;
+    }
+    //判断增加或者减少主数据库信息是否存在
+    private boolean isInfoExist(String info) throws SQLException {
+        boolean result = false;
+        String x = info;
+        dbOperation isInfoExistDb = new dbOperation();
+        isInfoExistDb.DBConnect();
+        String sql = "select * from maint where info='" + x + "'";
+        ResultSet rs = isInfoExistDb.DBSqlQuery(x);
+        if (rs.next()) {
+            result = true;
+        }
+        isInfoExistDb.DBClosed();
+        return result;
+    }
+    //core method
+    public void increaseMethod() throws SQLException {
+        boolean result = isInfoExist(info);
+        dbOperation increaseDb = new dbOperation();
+        if (result) {
+            increaseDb.DBConnect();
+            //update maint set amount=amount+"amount" where info="info" and store="store"
+            String sql = "update maint set amount=amount+'" + amount + "' where info='" + info + "' and store='" + store + "'";
+            increaseDb.DBSqlExe(sql);
+            increaseDb.DBClosed();
+        } else {
+            increaseDb.DBConnect();
+            //update maint set amount=amount+"amount" where info="info" and store="store"
+            String sql = "insert into maint(info,amount,store) values('" + info + "','" + amount + "','" + store + "')";
+            increaseDb.DBSqlExe(sql);
+            increaseDb.DBClosed();
+        }
+    }
+    //应当放到模块入库之前进行判断
+    public void decreaseMethod() throws SQLException {
+        boolean result = isInfoExist(info);
+        dbOperation decreaseDb = new dbOperation();
+        if (result) {
+            decreaseDb.DBConnect();
+            //update maint set amount=amount+"amount" where info="info" and store="store"
+            String sql = "update maint set amount=amount-'" + amount + "' where info='" + info + "' and store='" + store + "'";
+            decreaseDb.DBSqlExe(sql);
+            decreaseDb.DBClosed();
+        }else{
+            
+            JOptionPane.showConfirmDialog(null, "您所销售的货品:"+info+"不存在！");
+        }
+    }
+}
---
+++ JWMS/src/jwms/input2Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jwms;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrator
+ */
+public class input2Main {
+
+    private int year;
+    private int month;
+    private int day;
+    private String id;
+    private String info;
+    private String color = "";
+    private String size = "";
+    private double inPrice = 0;
+    private double outPrice = 0;
+    private String store;
+    private int amount = 0;
+
+    public void transmit2InputT() {
+        try {
+            dbOperation t2Input = new dbOperation();
+            t2Input.DBConnect();
+            String sql;
+            sql = "insert into inputt values('" + id + "','" + year + "','" + month + "','" + day + "','" + info + "','" + amount + "','" + color + "','" + size + "','" + store + "''" + inPrice + "','" + outPrice + "')";
+            t2Input.DBSqlExe(sql);
+            t2Input.DBClosed();
+        } catch (SQLException ex) {
+            Logger.getLogger(input2Main.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "数据未能写入，请检查是否正确设置数据库，如依旧有问题请与作者联系");
+        }
+    }
+}
