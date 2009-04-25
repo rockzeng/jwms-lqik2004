3d8f8e2d7fe4d1e82d8cb4309e7a4389a2b9b06b
lqik2004 <lqik2004@0a4000ee-19f5-11de-b69a-45dc1c5e4070>
2009-04-25 13:28:08 +0000

    更新版本号到v0.11

--- JWMS/src/jwms/about.java
+++ JWMS/src/jwms/about.java
@@ -33,10 +33,10 @@ class aboutDialog extends JFrame{
 
     public aboutDialog() {
         String version=null;
-        version="0.10_Alpha";
+        version="0.11_Alpha";
         JLabel l1=new JLabel("Version：'"+version+"'");
         JLabel l2=new JLabel("Author：刘潮");
-        JLabel l3=new JLabel("Update：2009-4-23");
+        JLabel l3=new JLabel("Update：2009-4-25");
         JLabel l4=new JLabel("Licence：GPL3");
         Box h0=Box.createVerticalBox();
         h0.add(Box.createVerticalStrut(5));
