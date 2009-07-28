592e2d79484acca074bdd3c7dc439f35a4fd549d
lqik2004 <lqik2004@0a4000ee-19f5-11de-b69a-45dc1c5e4070>
2009-07-28 12:18:30 +0000

    初步完成对数据库的选择，支持mysql 和sqlserver

--- jwms_linux_eclipse/src/com/res0w/jwms/method/DBOperation.java
+++ jwms_linux_eclipse/src/com/res0w/jwms/method/DBOperation.java
@@ -1,66 +1,79 @@
 package com.res0w.jwms.method;
 
 import java.sql.*;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * 
  * @author Administrator
  */
 public class DBOperation {
 
-    private String SQL;
-    private Connection conn;
-    private Statement stmt;
-    private ResultSet rs = null;
+	private String SQL;
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs = null;
+	private String sqlType;
 
-    public static void DBcleared(String x){
-        
-    }
+	public DBOperation() {
+		sqlType = PropertiesRW.proIDMakeRead("sqltype").toUpperCase();
+	}
 
-    public void DBConnect() {
+	
+	public static void DBcleared(String x) {
 
-        try {
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-            String url = "jdbc:odbc:jwms";
-            String user = "sa";
-            String password = "";
-            conn = DriverManager.getConnection(url, user, password);
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-        } catch (Exception e) {
-            e.printStackTrace();
+	}
 
-        }
-    }
+	public void DBConnect() {
+		String url;
+		String user;
+		String password;
+		try {
+			if ("MYSQL".equals(sqlType)) {
+				url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=gb2312";
+				user = "root";
+				password = "";
+				Class.forName("com.mysql.jdbc.Driver");
+			} else {
+				Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+				url = "jdbc:odbc:jwms";
+				user = "sa";
+				password = "";
+			}
+			conn = DriverManager.getConnection(url, user, password);
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+		} catch (Exception e) {
+			System.err.println("Connection the database is wrong  !!");
+		}
+	}
 
-    public void DBClosed() {
-        try {
-            //rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(DBOperation.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	public void DBClosed() {
+		try {
+			// rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(DBOperation.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+	}
 
-    public ResultSet DBSqlQuery(String sql) throws SQLException {
-        SQL = sql;
-        rs = stmt.executeQuery(sql);
-        return rs;
-    }
+	public ResultSet DBSqlQuery(String sql) throws SQLException {
+		SQL = sql;
+		rs = stmt.executeQuery(sql);
+		return rs;
+	}
 
-    public void DBSqlExe(String sql) throws SQLException {
-        SQL = sql;
-        boolean r;
-        r = stmt.execute(sql);
+	public void DBSqlExe(String sql) throws SQLException {
+		SQL = sql;
+		boolean r;
+		r = stmt.execute(sql);
 
-    }
+	}
 
-    public ResultSet DBReturnRS() {
-        return rs;
-    }
+	public ResultSet DBReturnRS() {
+		return rs;
+	}
 }
-
